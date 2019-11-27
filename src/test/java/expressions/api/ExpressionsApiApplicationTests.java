108b276fe928ec19bcaeefe8351acaf635efbcd5
Martin Straus <martin.straus@fit.com.ar>
2019-11-27 18:57:04 -0300

    Initial version.

---
+++ src/test/java/expressions/api/ExpressionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package expressions.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
